91fa32c804cafc1ad5c359caa907d8acdbb696e2
Vincenzo M <vincenzomaiorana@gmail.com>
2023-02-10 14:28:01 +0100

    fetchbyquery on gallery mount

--- src/components/CustomGallery.jsx
+++ src/components/CustomGallery.jsx
@@ -1,30 +1,36 @@
 import { Component } from "react";
-import { Col } from "react-bootstrap";
+import { SingleGalleryMovie } from "./SingleGalleryMovie";
 
 export class CustomGallery extends Component {
+  state = {
+    films: [],
+    isLoading: true,
+    error: false,
+  };
+
+  fetchByQuery = async (query) => {
+    try {
+      let queryResult = await fetch(process.env.REACT_APP_OMDBBASEURL + `?&s=${query}` + `&apikey=${process.env.REACT_APP_OMDBTOKEN}`);
+      let { Search: films } = await queryResult.json();
+
+      this.setState({ ...this.state, films });
+    } catch (e) {
+      alert(e);
+    }
+  };
+
+  componentDidMount() {
+    this.fetchByQuery(this.props.title);
+  }
+
   render() {
     return (
       <>
-        <h4>{this.props.title}</h4>
-        <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-4 row-cols-xl-6 mb-4 no-gutters text-center">
-          <Col className="mb-2 px-1">
-            <img className="img-fluid" src="assets/1.png" alt="movie picture" />
-          </Col>
-          <Col className="mb-2 px-1">
-            <img className="img-fluid" src="assets/2.png" alt="movie picture" />
-          </Col>
-          <Col className="mb-2 px-1">
-            <img className="img-fluid" src="assets/3.png" alt="movie picture" />
-          </Col>
-          <Col className="mb-2 px-1">
-            <img className="img-fluid" src="assets/4.png" alt="movie picture" />
-          </Col>
-          <Col className="mb-2 px-1">
-            <img className="img-fluid" src="assets/5.png" alt="movie picture" />
-          </Col>
-          <Col className="mb-2 px-1">
-            <img className="img-fluid" src="assets/6.png" alt="movie picture" />
-          </Col>
+        <h4>Perchè hai cercato: {this.props.title}</h4>
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-4 row-cols-xl-6 mb-4 no-gutters text-center xscroll">
+          {this.state.films.map((film) => (
+            <SingleGalleryMovie {...film} key={film.imdbID} />
+          ))}
         </div>
       </>
     );
